b7af4e87f97e4b91f352fc0eabbd10cafdc00579
James_McNeill <jmcneill06@outlook.com>
2021-11-23 16:47:12 +0000

    Update patindex.sql

--- SQL/SQLServer/Strings/patindex.sql
+++ SQL/SQLServer/Strings/patindex.sql
@@ -4,3 +4,11 @@ SELECT	Department,
 FROM Medium.dbo.[HR-Employee-Attrition]
 -- Look for Job Roles that have an "b" followed by 0 or more letters and then have a "y"
 WHERE PATINDEX('%b%y%', JobRole) > 0;
+
+
+SELECT	DISTINCT JobRole,
+		Department,
+		BusinessTravel
+FROM Medium.dbo.[HR-Employee-Attrition]
+-- Look for Job Roles that contain one of the letters: "x", "w", "q"
+WHERE PATINDEX('%[xwq]%', JobRole) > 0;
