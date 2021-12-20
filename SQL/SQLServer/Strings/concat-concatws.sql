a41b037df4ce8ceedf41b6e85ecf27959aee02c6
James_McNeill <jmcneill06@outlook.com>
2021-12-20 16:17:06 +0000

    Update concat-concatws.sql

--- SQL/SQLServer/Strings/concat-concatws.sql
+++ SQL/SQLServer/Strings/concat-concatws.sql
@@ -1,17 +1,15 @@
-DECLARE @string1 NVARCHAR(100) = 'Chocolate with beans from';
-DECLARE @string2 NVARCHAR(100) = 'has a cocoa percentage of';
+DECLARE @string1 NVARCHAR(100) = 'Employees from';
+DECLARE @string2 NVARCHAR(100) = 'have the role of';
+DECLARE @string3 NVARCHAR(100) = 'with job satisfaction of';
 
-SELECT
-bean_type,
-bean_origin,
-cocoa_percent,
-- Create a message by concatenating values with "+"
-@string1 + ' ' + bean_origin + ' ' + @string2 + ' ' + CAST(cocoa_percent AS nvarchar) AS message1,
-- Create a message by concatenating values with "CONCAT()"
-CONCAT(@string1, ' ', bean_origin, ' ', @string2, ' ', cocoa_percent) AS message2,
-- Create a message by concatenating values with "CONCAT_WS()"
-CONCAT_WS(' ', @string1, bean_origin, @string2, cocoa_percent) AS message3
-FROM ratings
-WHERE
-company = 'Ambrosia'
-AND bean_type <> 'Unknown';
+SELECT TOP (10)
+hr.EducationField,
+hr.JobRole,
+hr.JobSatisfaction,
+ -- Create a message by concatenating values with "+"
+@string1 + ' ' + hr.EducationField + ' ' + @string2 + ' ' + hr.JobRole + ' ' + @string3 + ' ' + CAST(hr.JobSatisfaction AS NVARCHAR) AS message1,
+ -- Create a message by concatenating values with "CONCAT()"
+CONCAT(@string1, ' ', hr.EducationField, ' ', @string2, ' ', hr.JobRole, ' ', @string3, ' ', hr.JobSatisfaction) AS message2,
+ -- Create a message by concatenating values with "CONCAT_WS()"
+CONCAT_WS(' ', @string1, hr.Education, @string2, hr.JobRole, @string3, hr.JobSatisfaction) AS message3
+FROM Medium.dbo.[HR-Employee-Attrition] as hr;
